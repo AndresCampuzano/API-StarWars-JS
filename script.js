c49ead4dc677db9d11a2b6f0d0dd4ef351e11607
unknown <campuxano88@gmail.com>
2019-12-15 08:10:24 -0500

    Here I made multiple requests

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const PEOPLE_URL = 'people/:id';
 
 // https://platzi.com/clases/1339-fundamentos-javascript/12959-callbacks82-6/
 
-const lukeurl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
 const opts = { crossDomain: true };
 
 const onPeopleResponse = function (persona) {
-    console.log(`Hola, yo soy ${persona.name} y peso ${persona.mass}`)
+    console.log(`Hola, yo soy ${persona.name} y peso ${persona.mass} kg`)
 };
 
-$.get(lukeurl, opts, onPeopleResponse);
+function obtenerPersonaje (id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, onPeopleResponse);
+}
+
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
