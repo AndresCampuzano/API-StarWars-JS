3faec1a2ad6077f9c3a82dab30a82d7ac2f87660
unknown <campuxano88@gmail.com>
2019-12-15 09:22:02 -0500

    Handling error in getting the data using .fail

--- script.js
+++ script.js
@@ -8,21 +8,27 @@ const opts = { crossDomain: true };
 function obtenerPersonaje (id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
 
-    $.get(url, opts, function (persona) {
-        console.log(`Hola, yo soy ${persona.name}`)
-
-        if(callback){
-            callback();
-        }
+    $.get(url, opts, callback).fail(function() {
+        console.log(`Error, unable to get id: ${id}`)
     });
 }
 
 // Esto es un callback, para controlar el asincronismo y que los datos sean mostrados en orden.
-obtenerPersonaje(1, function () {
-    obtenerPersonaje(2, function () {
-        obtenerPersonaje(3, function () {
-            obtenerPersonaje(4, function () {
-                obtenerPersonaje(5)
+obtenerPersonaje(1, function (personaje) {
+    console.log(`Hola, yo soy ${personaje.name}`)
+
+    obtenerPersonaje(2, function (personaje) {
+        console.log(`Hola, yo soy ${personaje.name}`)
+
+        obtenerPersonaje(3, function (personaje) {
+            console.log(`Hola, yo soy ${personaje.name}`)
+
+            obtenerPersonaje(4, function (personaje) {
+                console.log(`Hola, yo soy ${personaje.name}`)
+
+                obtenerPersonaje(5, function (personaje) {
+                    console.log(`Hola, yo soy ${personaje.name}`)
+                })
             })
         })
     })
