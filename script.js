52451db290ec6169186853d3087de965d1bac7d1
unknown <campuxano88@gmail.com>
2019-12-15 08:23:58 -0500

    Here I'm handling callbacks to show the data in a controled way.

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
+const opts = { crossDomain: true };
 
 // https://platzi.com/clases/1339-fundamentos-javascript/12959-callbacks82-6/
 
-const opts = { crossDomain: true };
 
-const onPeopleResponse = function (persona) {
-    console.log(`Hola, yo soy ${persona.name} y peso ${persona.mass} kg`)
-};
+function obtenerPersonaje (id, callback) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
 
-function obtenerPersonaje (id) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, onPeopleResponse);
+    $.get(url, opts, function (persona) {
+        console.log(`Hola, yo soy ${persona.name}`)
+
+        if(callback){
+            callback();
+        }
+    });
 }
 
-obtenerPersonaje(1);
-obtenerPersonaje(2);
-obtenerPersonaje(3);
+// Esto es un callback, para controlar el asincronismo y que los datos sean mostrados en orden.
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3, function () {
+            obtenerPersonaje(4, function () {
+                obtenerPersonaje(5)
+            })
+        })
+    })
+})
