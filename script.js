23075887522922d103688cbd1c4b026c8615c839
unknown <campuxano88@gmail.com>
2019-12-17 21:12:15 -0500

    Learning chained promises

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-const API_URL = 'https://swapi.co/api/';
-const PEOPLE_URL = 'people/:id';
+const API_URL = "https://swapi.co/api/";
+const PEOPLE_URL = "people/:id";
 const opts = { crossDomain: true };
 
 // https://platzi.com/clases/1339-fundamentos-javascript/12959-callbacks82-6/
 
-
-function obtenerPersonaje (id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
-
-    $.get(url, opts, callback).fail(function() {
-        console.log(`Error, unable to get id: ${id}`)
+function obtenerPersonaje(id) {
+  return new Promise(function(resolve, reject) {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+    $.get(url, opts, function(data) {
+      resolve(data);
+    }).fail(function() {
+      reject(id);
     });
+  });
 }
 
-// Esto es un callback, para controlar el asincronismo y que los datos sean mostrados en orden.
-obtenerPersonaje(1, function (personaje) {
-    console.log(`Hola, yo soy ${personaje.name}`)
+function onError(id) {
+    console.log(`Sucedio un error al obtener el personaje ${id}`);
+}
 
-    obtenerPersonaje(2, function (personaje) {
-        console.log(`Hola, yo soy ${personaje.name}`)
+obtenerPersonaje(1).then(function(personaje) {
+    console.log(`El personaje 1 es ${personaje.name}`);
+}) .catch(onError)
 
-        obtenerPersonaje(3, function (personaje) {
-            console.log(`Hola, yo soy ${personaje.name}`)
-
-            obtenerPersonaje(4, function (personaje) {
-                console.log(`Hola, yo soy ${personaje.name}`)
-
-                obtenerPersonaje(5, function (personaje) {
-                    console.log(`Hola, yo soy ${personaje.name}`)
-                })
-            })
-        })
-    })
-})
 
 // for (var i = 1; i <= 10; i++) {
 //     obtenerPersonaje(i, function (personaje) {
